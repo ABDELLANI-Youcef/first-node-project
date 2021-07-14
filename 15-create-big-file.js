7cd100a8eb1715dfaf09a66b26c4703ae60bfb13
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-07-14 15:21:29 +0100

    use stream to serve data

--- 15-create-big-file.js
+++ 15-create-big-file.js
@@ -1,4 +1,4 @@
 const { writeFileSync } = require('fs')
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 100000; i++) {
   writeFileSync('./content/big.txt', `hello world ${i}\n`, { flag: 'a' })
 }
--- 17-streams.js
+++ 17-streams.js
@@ -1,12 +1,12 @@
-const {createReadStream} = require('fs');
+const { createReadStream } = require('fs');
 
-const stream = createReadStream('./content/big.txt',{
+const stream = createReadStream('./content/big.txt', {
   highWaterMark: 90000,
   //encoding: 'utf8'
 });
 
-stream.on('data',(result)=>{
+stream.on('data', (result) => {
   console.log(result);
 });
 
-stream.on('error',(err)=>console.log(err));
+stream.on('error', (err) => console.log(err));
